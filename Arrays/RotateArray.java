e979436839ff2ab45b04de8ccd094b4b0fdef6bf
Muskan Kumari <muskan91025kumari@gmail.com>
2026-03-07 16:54:21 +0530

    Update K_RotateArray.java

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -1,24 +1,34 @@
 package Arrays;
 
-public class RotateArray {
-    public static void main(String[] args){
-        int [] a = {1,2,3,4,5,6,7};
-        int k=3;
-        for (int i=0;i<a.length;i++){
-            System.out.print(a[i]+"\t");
-        }
-        System.out.println();
-        System.out.println();
-        for (int i=0;i<k;i++){
-            for (int j=a.length-1;j>0;j--){
-                int t = a[j-1];
-                a[j-1] = a[j];
-                a[j] = t;
+import java.util.Scanner;
 
-            }
+public class K_RotateArray {
+    public static int[] rotate(int [] arr , int k){
+        int [] ans = new int[arr.length];
+        int pos = 0;
+        int n = arr.length;
+        k = k % n;
+        for (int i = n-k;i<n;i++){
+            ans[pos] = arr[i];
+            pos++;
         }
-        for (int i=0;i<a.length;i++){
-            System.out.print(a[i]+"\t");
+        for (int i=0;i<n-k;i++){
+            ans[pos] = arr[i];
+            pos++;
         }
+        return ans;
+    }
+    public static void main(String[] args){
+        int []  arr = {1,2,3,4,7};
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the k : ");
+        int k = sc.nextInt();
+        int [] ans = rotate(arr,k);
+        for (int i=0;i<ans.length;i++){
+            System.out.print(ans[i]);
+        }
+
     }
 }
+
+
