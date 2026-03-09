01c8efaf718cbe3eb5671142b422e5901b065917
Muskan Kumari <muskan91025kumari@gmail.com>
2026-03-09 16:06:22 +0530

    Update Transpose_matrix.java

--- Arrays_2D/Transpose_matrix.java
+++ Arrays_2D/Transpose_matrix.java
@@ -1,21 +1,15 @@
-package Arrays_2D;
-
-import java.util.Scanner;
-
-public class Transpose_matrix {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int [][] matrix = new int[2][3];
-        for (int i=0;i<2;i++){
-            for (int j=0;j<3;j++){
-                matrix[i][j]= sc.nextInt();
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int [][] ans = new int[matrix[0].length][matrix.length];
+          
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+              ans[j][i] = matrix[i][j];
             }
+           
         }
-        for (int i=0;i<3;i++){
-            for (int j=0;j<2;j++){
-                System.out.print(matrix[j][i]);
-            }
-            System.out.println();
-        }
+        return ans;
+        
     }
 }
+
